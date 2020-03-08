08243c26f08a4be57348e2cfc06385c7c725cfa7
fjlacruz <idsistemas15@gmail.com>
2020-03-07 22:11:23 -0300

    Actualizando

--- controllers/project.js
+++ controllers/project.js
@@ -1,157 +1,203 @@
-'use strict'
+"use strict";
 
-var Project = require('../models/project');
-var fs = require('fs');
-var path = require('path');
+var Project = require("../models/project");
+var fs = require("fs");
+var path = require("path");
 
 var controller = {
-	
-	home: function(req, res){
-		return res.status(200).send({
-			message: 'Soy la home'
-		});
-	},
+  home: function(req, res) {
+    return res.status(200).send({
+      message: "API Node Mongo DB",
+      created_by: "Javier La Cruz",
+      version: "1.0",
+      estatus: "OK"
+    });
+  },
 
-	test: function(req, res){
-		return res.status(200).send({
-			message: "Soy el metodo o accion test del controlador de project"
-		});
-	},
+  test: function(req, res) {
+    return res.status(200).send({
+      message: "Soy el metodo o accion test del controlador de project"
+    });
+  },
 
-	saveProject: function(req, res){
-		var project = new Project();
+  saveProject: function(req, res) {
+    var project = new Project();
 
-		var params = req.body;
-		project.name = params.name;
-		project.description = params.description;
-		project.category = params.category;
-		project.year = params.year;
-		project.langs = params.langs;
-		project.image = null;
+    var params = req.body;
+    project.name = params.name;
+    project.description = params.description;
+    project.category = params.category;
+    project.year = params.year;
+    project.langs = params.langs;
+    project.image = null;
 
-		project.save((err, projectStored) => {
-			if(err) return res.status(500).send({message: 'Error al guardar el documento.'});
+    project.save((err, projectStored) => {
+      if (err)
+        return res
+          .status(500)
+          .send({ message: "Error al guardar el documento." });
 
-			if(!projectStored) return res.status(404).send({message: 'No se ha podido guardar el proyecto.'});
+      if (!projectStored)
+        return res
+          .status(404)
+          .send({ message: "No se ha podido guardar el proyecto." });
 
-			return res.status(200).send({project: projectStored});
-		});
-	},
+      return res.status(200).send({ project: projectStored });
+    });
+  },
 
-	getProject: function(req, res){
-		var projectId = req.params.id;
+  getProject: function(req, res) {
+    var projectId = req.params.id;
 
-		if(projectId == null) return res.status(404).send({message: 'El proyecto no existe.'});
+    if (projectId == null)
+      return res.status(404).send({ message: "El proyecto no existe." });
 
-		Project.findById(projectId, (err, project) => {
+    Project.findById(projectId, (err, project) => {
+      if (err)
+        return res
+          .status(500)
+          .send({ message: "Error al devolver los datos." });
 
-			if(err) return res.status(500).send({message: 'Error al devolver los datos.'});
+      if (!project)
+        return res.status(404).send({ message: "El proyecto no existe." });
 
-			if(!project) return res.status(404).send({message: 'El proyecto no existe.'});
+      return res.status(200).send({
+        project
+      });
+    });
+  },
 
-			return res.status(200).send({
-				project
-			});
+  getProjects: function(req, res, next) {
+    console.log(req);
+    Project.find({})
+      .sort("-year")
+      .exec((err, projects) => {
+        if (err)
+          return res
+            .status(500)
+            .send({ message: "Error al devolver los datos." });
 
-		});
-	},
+        if (!projects)
+          return res
+            .status(404)
+            .send({ message: "No hay projectos que mostrar." });
+        // if (projects == "")
+        //   return res.send({ message: "No hay projectos que mostrar." });
 
-	getProjects: function(req, res){
+        return res.status(200).send({ projects });
+      });
+  },
 
-		Project.find({}).sort('-year').exec((err, projects) => {
+  updateProject: function(req, res) {
+    var projectId = req.params.id;
+    var update = req.body;
 
-			if(err) return res.status(500).send({message: 'Error al devolver los datos.'});
+    Project.findByIdAndUpdate(
+      projectId,
+      update,
+      { new: true },
+      (err, projectUpdated) => {
+        if (err)
+          return res.status(500).send({ message: "Error al actualizar" });
 
-			if(!projects) return res.status(404).send({message: 'No hay projectos que mostrar.'});
+        if (!projectUpdated)
+          return res
+            .status(404)
+            .send({ message: "No existe el proyecto para actualizar" });
 
-			return res.status(200).send({projects});
-		});
+        return res.status(200).send({
+          project: projectUpdated
+        });
+      }
+    );
+  },
 
-	},
+  deleteProject: function(req, res) {
+    var projectId = req.params.id;
 
-	updateProject: function(req, res){
-		var projectId = req.params.id;
-		var update = req.body;
+    Project.findByIdAndRemove(projectId, (err, projectRemoved) => {
+      if (err)
+        return res
+          .status(500)
+          .send({ message: "No se ha podido borrar el proyecto" });
 
-		Project.findByIdAndUpdate(projectId, update, {new:true}, (err, projectUpdated) => {
-			if(err) return res.status(500).send({message: 'Error al actualizar'});
+      if (!projectRemoved)
+        return res
+          .status(404)
+          .send({ message: "No se puede eliminar ese proyecto." });
 
-			if(!projectUpdated) return res.status(404).send({message: 'No existe el proyecto para actualizar'});
+      return res.status(200).send({
+        project: projectRemoved
+      });
+    });
+  },
 
-			return res.status(200).send({
-				project: projectUpdated
-			});
-		});
+  uploadImage: function(req, res) {
+    var projectId = req.params.id;
+    var fileName = "Imagen no subida...";
 
-	},
+    if (req.files) {
+      var filePath = req.files.image.path;
+      var fileSplit = filePath.split("/");
+      var fileName = fileSplit[1];
+      var extSplit = fileName.split(".");
+      var fileExt = extSplit[1];
+      console.log(extSplit);
 
-	deleteProject: function(req, res){
-		var projectId = req.params.id;
+      if (
+        fileExt == "png" ||
+        fileExt == "jpg" ||
+        fileExt == "jpeg" ||
+        fileExt == "gif"
+      ) {
+        Project.findByIdAndUpdate(
+          projectId,
+          { image: fileName },
+          { new: true },
+          (err, projectUpdated) => {
+            if (err)
+              return res
+                .status(500)
+                .send({ message: "La imagen no se ha subido" });
 
-		Project.findByIdAndRemove(projectId, (err, projectRemoved) => {
-			if(err) return res.status(500).send({message: 'No se ha podido borrar el proyecto'});
+            if (!projectUpdated)
+              return res.status(404).send({
+                message: "El proyecto no existe y no se ha asignado la imagen"
+              });
 
-			if(!projectRemoved) return res.status(404).send({message: "No se puede eliminar ese proyecto."});
+            return res.status(200).send({
+              project: projectUpdated
+            });
+          }
+        );
+      } else {
+        fs.unlink(filePath, err => {
+          return res
+            .status(200)
+            .send({ message: "La extensión no es válida....." });
+        });
+      }
+    } else {
+      return res.status(200).send({
+        message: fileName
+      });
+    }
+  },
 
-			return res.status(200).send({
-				project: projectRemoved
-			});
-		});
-	},
+  getImageFile: function(req, res) {
+    var file = req.params.image;
+    var path_file = "./uploads/" + file;
 
-	uploadImage: function(req, res){
-		var projectId = req.params.id;
-		var fileName = 'Imagen no subida...';
-
-		if(req.files){
-			var filePath = req.files.image.path;
-			var fileSplit = filePath.split('/');
-			var fileName = fileSplit[1];
-			var extSplit = fileName.split('.');
-			var fileExt = extSplit[1];
-            console.log(extSplit);
-
-			if(fileExt == 'png' || fileExt == 'jpg' || fileExt == 'jpeg' || fileExt == 'gif'){
-
-				Project.findByIdAndUpdate(projectId, {image: fileName}, {new: true}, (err, projectUpdated) => {
-					if(err) return res.status(500).send({message: 'La imagen no se ha subido'});
-
-					if(!projectUpdated) return res.status(404).send({message: 'El proyecto no existe y no se ha asignado la imagen'});
-
-					return res.status(200).send({
-						project: projectUpdated
-					});
-				});
-
-			}else{
-				fs.unlink(filePath, (err) => {
-					return res.status(200).send({message: 'La extensión no es válida.....'});
-				});
-			}
-
-		}else{
-			return res.status(200).send({
-				message: fileName
-			});
-		}
-
-	},
-
-	getImageFile: function(req, res){
-		var file = req.params.image;
-		var path_file = './uploads/'+file;
-
-		fs.exists(path_file, (exists) => {
-			if(exists){
-				return res.sendFile(path.resolve(path_file));
-			}else{
-				return res.status(200).send({
-					message: "No existe la imagen..."
-				});
-			}
-		});
-	}
-
+    fs.exists(path_file, exists => {
+      if (exists) {
+        return res.sendFile(path.resolve(path_file));
+      } else {
+        return res.status(200).send({
+          message: "No existe la imagen..."
+        });
+      }
+    });
+  }
 };
 
 module.exports = controller;
